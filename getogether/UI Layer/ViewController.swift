3dd5d47f00e0c0ed6a53406e7cc71a25806e5fd5
Imran Hajiyev <haciyev008@gmail.com>
2023-04-12 11:23:25 +0400

    Initial Commit

---
+++ getogether/UI Layer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  getogether
+//
+//  Created by Imran Hajiyev on 10.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
